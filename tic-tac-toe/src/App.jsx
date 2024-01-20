2efb00909ceb45249c631409d54fc4749fe05bff
Santiago dos Santos <santiiagodossantos@icloud.com>
2024-01-20 16:35:34 -0300

    Winner logic

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,9 +1,26 @@
 import { useState } from "react";
 
 const TURNS = {
-  X: "X",
-  O: "O",
+  X: "❌",
+  O: "⚪",
 };
+
+const END_GAME = {
+  X: "X wins",
+  O: "O wins",
+  DRAW: "Draw",
+};
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? "is-selected" : ""}`;
@@ -20,14 +37,38 @@ function App() {
 
   const [turn, setTurn] = useState(TURNS.X);
 
+  const [winner, setWinner] = useState(null);
+
   const updateBoard = (index) => {
-    console.log(index);
+    if (board[index] !== null || winner) return;
+
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
 
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+      return;
+    }
+
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+  };
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+
+    return null;
   };
 
   return (
