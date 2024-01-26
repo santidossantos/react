fefeb95b7e35b831a9f2bfb4e2035e13e770887b
Santiago dos Santos <santiiagodossantos@icloud.com>
2024-01-25 22:17:38 -0300

    Error

--- prueba-tecnica-fetching/src/App.jsx
+++ prueba-tecnica-fetching/src/App.jsx
@@ -25,18 +25,29 @@ function App() {
   // Recuperamos el fact con este efecto
   useEffect(() => {
     fetch(CAT_FACT_ENDPOINT)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) throw new Error("Fetch error");
+
+        return res.json();
+      })
       .then((data) => {
         const { fact } = data;
         setFact(fact);
+      })
+      .catch(() => {
+        // Entra si hay error en la respuesta
+        // o hay  error en la peticion 
+       // (por defecto fetch solo evalua esto ultimo)
+       // pero como hicimos throw error tambien contemplamos
+       // si la peticion fue bien pero la respuesta esta mal (bad request por ejemplo)
       });
   }, []);
 
   /* Usamos otro efecto para recuperar la imagen una vez que ya 
      se ha recuperado el fact del primer efecto */
   useEffect(() => {
-    if(!fact) return
-    
+    if (!fact) return;
+
     const firstThreeWords = fact.split(" ").slice(0, 3).join("%20");
 
     fetch(CAT_IMG_URL_ENDPOINT + firstThreeWords).then((response) => {
