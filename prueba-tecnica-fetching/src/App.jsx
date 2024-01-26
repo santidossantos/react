1f1e9d1d9ac6e6d4597ad378c214834f0f50eebf
Santiago dos Santos <santiiagodossantos@icloud.com>
2024-01-25 22:26:10 -0300

    Refactoring

--- prueba-tecnica-fetching/src/App.jsx
+++ prueba-tecnica-fetching/src/App.jsx
@@ -15,41 +15,31 @@ Enunciado:
 */
 
 const CAT_FACT_ENDPOINT = "https://catfact.ninja/fact";
-
 const CAT_IMG_URL_ENDPOINT = "https://cataas.com/cat/says/";
 
 function App() {
   const [fact, setFact] = useState();
   const [imageUrl, setImageUrl] = useState();
 
-  // Recuperamos el fact con este efecto
   useEffect(() => {
     fetch(CAT_FACT_ENDPOINT)
       .then((res) => {
         if (!res.ok) throw new Error("Fetch error");
-
         return res.json();
       })
       .then((data) => {
         const { fact } = data;
         setFact(fact);
       })
-      .catch(() => {
-        // Entra si hay error en la respuesta
-        // o hay  error en la peticion 
-       // (por defecto fetch solo evalua esto ultimo)
-       // pero como hicimos throw error tambien contemplamos
-       // si la peticion fue bien pero la respuesta esta mal (bad request por ejemplo)
+      .catch((err) => {
+        console.log(err);
       });
   }, []);
 
-  /* Usamos otro efecto para recuperar la imagen una vez que ya 
-     se ha recuperado el fact del primer efecto */
   useEffect(() => {
     if (!fact) return;
 
     const firstThreeWords = fact.split(" ").slice(0, 3).join("%20");
-
     fetch(CAT_IMG_URL_ENDPOINT + firstThreeWords).then((response) => {
       const { url } = response;
       setImageUrl(url);
@@ -59,7 +49,6 @@ function App() {
   return (
     <main>
       <h1>App de gatitos</h1>
-
       <section>
         {fact && <p>{fact}</p>}
         {imageUrl && <img src={imageUrl} alt="cat"></img>}
