7583231f0f6e883d45e6c3966705347af4f6cdb2
Santiago dos Santos <santiiagodossantos@icloud.com>
2024-02-02 13:15:39 -0300

    Mock render

--- prueba-tecnica-peliculas/src/App.jsx
+++ prueba-tecnica-peliculas/src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
+import responseMovies from "./mocks/with-results.json";
+import withoutResults from "./mocks/no-results.json";
 
 function App() {
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
+
   return (
     <div className="App">
       <header>
@@ -11,7 +16,19 @@ function App() {
         </form>
       </header>
 
-      <main>Resultados...</main>
+      <main>
+        {hasMovies && (
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt={movie.Title}></img>
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
     </div>
   );
 }
